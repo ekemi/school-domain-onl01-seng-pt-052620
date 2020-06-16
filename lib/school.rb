ca49e288318225fbb887c15ff6f17d19241a213b
edmundo papi <edmundopierre@yahoo.com>
2020-06-16 11:48:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,9 +10,14 @@ class School
 end
 
 def add_student(student_name, grade)
+
+  @roster.each do |key, value|
+    if @roster.include?(grade)
+        @roster[key] << student_name
+    else
     @roster[grade] = []
     @roster[grade] << student_name
-
+  end
 end
 
 end
