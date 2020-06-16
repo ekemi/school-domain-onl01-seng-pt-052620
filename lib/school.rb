ea60756475b0363030f50908901ee12a078153ce
edmundo papi <edmundopierre@yahoo.com>
2020-06-16 11:33:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+
+
+class School
+  attr_accessor :roster
+
+ def initialize(roster)
+   @roster = {}
+end
+
+def add_student(name, grade)
+  # @roste[grade] = []
+  @roster.each do |grade, name|
+  if @roster.include?(grade)
+    @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+end
+  #@roster [grade] << name
+
+end
+
+end
